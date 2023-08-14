2c18c970cf80b49f6180132f0f9009e9dd3c5418
spatte30 <36490221+spatte30@users.noreply.github.com>
2023-08-14 14:25:16 -0600

    Update Deploy-Application.ps1

--- Deploy-Application.ps1
+++ Deploy-Application.ps1
@@ -120,7 +120,7 @@ Try {
 		##*===============================================
 		[string]$installPhase = 'Pre-Installation'
 
-		## Show Welcome Message, close Internet Explorer if required, allow up to 3 deferrals, verify there is enough disk space to complete the install, and persist the prompt
+		## Show Welcome Message, close Internet Explorer if required, allow up to 3 deferrals, verify there is enough disk space to complete the install, and persist the prompt.
 		Show-InstallationWelcome -CloseApps 'ARCHICAD,ARCHICAD Starter,BIMx,GRAPHISOFT License Manager Tool' -CheckDiskSpace -PersistPrompt
 
 		## Show Progress Message (with the default message)
